26a06cf217efca023e3abee0e41f8b8a8e669cac
daniel-bitman <bitman.daniel@gmail.com>
2018-09-13 15:19:00 +0300

    trying commit

--- try2.py
+++ try2.py
@@ -1,7 +1,13 @@
 #! /usr/bin/env python3
 
-num1=int(input("Enter a first number: "))
-num2=int(input("Enter a second number: "))
+#num1=int(input("Enter a first number: "))
+#num2=int(input("Enter a second number: "))
+
+name = input("Enter your name: ")
+height =float(input("Enter a height in meters: "))
+weight = int(input("Enter a weight in kg : "))
+def bmi(n='',h=1,w=0)
+    retrun w%(h**2)
 
 
 
